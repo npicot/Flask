5f532b43860f791f798a45a44278a38d430e51ea
picotn <natanael.picot@groupetcp.com>
2023-06-14 17:30:17 +0200

    en cours (fr <--> en)

---
+++ site_web/static/fr-en.js
@@ -0,0 +1,14 @@
+jQuery(function(){
+    $(function () {
+        $("#fr-en").click(function() {
+            if ( $(":lang(en)").hasClass("hide") ) {
+                $(":lang(fr)").addClass("hide");
+                $(":lang(en)").removeClass("hide");
+            }
+            else {
+                $(":lang(en)").addClass("hide");
+                $(":lang(fr)").removeClass("hide");
+            }
+        });
+    });
+});
