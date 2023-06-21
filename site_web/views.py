5a26791eaaa7ee7e5d434f18f53df373d1b41888
picotn <natanael.picot@groupetcp.com>
2023-06-21 09:05:07 +0200

    ajout date

--- site_web/views.py
+++ site_web/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+from datetime import datetime
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -13,7 +14,10 @@ def hello():
         cur.execute('UPDATE visitors SET nb_visitors = {};'.format(nb_visitors))
         print(nb_visitors)
 
-    return render_template('CV.html', nb_visitors = nb_visitors)
+    now = datetime.now()
+    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+
+    return render_template('CV.html', nb_visitors = nb_visitors, date_time = date_time)
 
 
 if __name__ == "__main__":
