6520ac0acb220f61975f0296da95616f507650c9
picotn <natanael.picot@groupetcp.com>
2023-06-22 16:30:53 +0200

    ajout fr-en + modif

--- site_web/static/fr-en.js
+++ site_web/static/fr-en.js
@@ -1,14 +1,12 @@
-jQuery(function(){
-    $(function () {
-        $("#fr-en").click(function() {
-            if ( $(":lang(en)").hasClass("hide") ) {
-                $(":lang(fr)").addClass("hide");
-                $(":lang(en)").removeClass("hide");
-            }
-            else {
-                $(":lang(en)").addClass("hide");
-                $(":lang(fr)").removeClass("hide");
-            }
-        });
+$(document).ready(function () {
+    $("#fr-en").click(function () {
+        if ($(":lang(en)").hasClass("hide")) {
+            $(":lang(fr)").addClass("hide");
+            $(":lang(en)").removeClass("hide");
+        }
+        else {
+            $(":lang(en)").addClass("hide");
+            $(":lang(fr)").removeClass("hide");
+        }
     });
 });
--- site_web/static/hover.js
+++ site_web/static/hover.js
@@ -15,16 +15,16 @@ jQuery(function(){
             });
 
             $(".circle2").hover(function () {
-                $(this).css("width", "120px");
-                $(this).css("height", "120px");
-                $(this).find("img").css("width", "65%");
-                $(this).find("img").css("height", "65%");
-            },
-                function(){
-                $(this).css("width", "100px");
-                $(this).css("height", "100px");
+                $(this).css("width", "90px");
+                $(this).css("height", "90px");
                 $(this).find("img").css("width", "60%");
                 $(this).find("img").css("height", "60%");
+            },
+                function(){
+                $(this).css("width", "75px");
+                $(this).css("height", "75px");
+                $(this).find("img").css("width", "50%");
+                $(this).find("img").css("height", "50%");
             });
         }
     });
