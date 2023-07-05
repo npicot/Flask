3ea9dbefb6693a68929fd36305e92268c9d1bd4d
picotn <natanael.picot@groupetcp.com>
2023-07-05 11:58:22 +0200

    fin animation

--- site_web/static/animation.js
+++ site_web/static/animation.js
@@ -5,25 +5,54 @@ $(document).ready(function () {
         $positon_contact = $("#contact_img").position(),
         $positon_motiv = $("#motiv_img").position()
 
-    $("#aboutme_img").animate({
-        "position": "absolute",
-        "left": "-=200",
-        "left": "+=500"
-    }, function () {
-        $("#resume_img").animate({
-            "left": "+=720px"
+    $("#aboutme_img, #resume_img, #works_img, #contact_img, #motiv_img").css({
+        position: "absolute",
+        "left": -200,
+    });
+    $("#aboutme_img, #aboutme_img2").animate({
+        "left": $positon_aboutme.left
+    }, 1500, function () {
+        $(this).css({
+           position: "static" 
         });
     });
-            
-    $("#works_img").animate({
-        "right": "+=520px"
-    }, function () {
-        $("#contact_img").animate({
-            "right": "+=720px"
-        }, function () {
-            $("#motiv_img").animate({
-                "right": "+=920px"
-            });
+    $("#resume_img, #resume_img2").animate({
+        "left": $positon_resume.left
+    }, 1500, function () {
+        $(this).css({
+           position: "static" 
         });
-    });/**/
+    });
+    $("#works_img, #works_img2").animate({
+        "left": $positon_works.left
+    }, 1500, function () {
+        $(this).css({
+           position: "static" 
+        });
+    });
+    $("#contact_img, #contact_img2").animate({
+        "left": $positon_contact.left
+    }, 1500, function () {
+        $(this).css({
+           position: "static" 
+        });
+    });
+    $("#motiv_img, #motiv_img2").animate({
+        "left": $positon_motiv.left
+    }, 1500, function () {
+        $(this).css({
+           position: "static" 
+        });
+
+        for (let index = 0; index < 1; index++) {
+            $("#fr-en").animate({
+                width: "40px",
+                height: "40px"
+            });
+            $("#fr-en").animate({
+                width: "80px",
+                height: "80px"
+            });
+        }
+    });
 });
--- site_web/views.py
+++ site_web/views.py
@@ -1,7 +1,6 @@
 import sqlite3 as sql
-import requests
 from flask import Flask, render_template
-
+from datetime import datetime
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -15,16 +14,11 @@ def hello():
         )
         nb_visitors += 1
         cur.execute("UPDATE visitors SET nb_visitors = {};".format(nb_visitors))
-        api_url = "https://api.api-ninjas.com/v1/worldtime?city=Paris"
-        response = requests.get(
-            api_url, headers={"X-Api-Key": "bBpuN8xpUaZyf2hBLvIQ6w==K0TJnWMgApZLHj8a"}
-        )
-        if response.status_code == requests.codes.ok:
-            response_json = response.json()
-        else:
-            print("Error:", response.status_code, response.text)
+
         return render_template(
-            "CV.html", nb_visitors=nb_visitors, response_json=response_json["datetime"]
+            "CV.html",
+            nb_visitors=nb_visitors,
+            date=datetime.now().strftime("%d/%m/%Y - %H:%M")
         )
 
 
